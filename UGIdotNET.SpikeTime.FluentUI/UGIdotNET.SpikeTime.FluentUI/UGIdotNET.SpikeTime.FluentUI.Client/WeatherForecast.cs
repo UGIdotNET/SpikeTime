b56a419067d42fb50ec2b136ffed3e0986f6866f
Alberto Mori <albi.mo87@hotmail.it>
2024-06-28 19:51:25 +0200

    Ep. 16 - Fluent UI

---
+++ UGIdotNET.SpikeTime.FluentUI/UGIdotNET.SpikeTime.FluentUI/UGIdotNET.SpikeTime.FluentUI.Client/WeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿namespace UGIdotNET.SpikeTime.FluentUI.Client;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public bool Selected { get; set; }
+}
