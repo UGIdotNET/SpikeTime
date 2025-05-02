edb2ff9342d9a20fbd8fc2c19d2b1797a72c5f80
albx <albi.mo87@hotmail.it>
2025-05-02 19:54:44 +0200

    Ep. 33 - Azure easy auth

---
+++ UGIdotNET.SpikeTime.EasyAuth/AspireApp/AspireApp.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireApp_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
